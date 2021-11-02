fdeac2a79f6d6cf19c12443905022e89d61fc392
pmontel.oliveira@gmail.com <pmontel.oliveira@gmail.com>
2021-11-02 16:33:06 -0300

    iniciando o projeto com backend laravel e front vue

---
+++ ecommerce-front/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    name:"Header",
+    props:{
+
+    },
+    components:{
+
+    },
+    data:{
+        
+    }
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
